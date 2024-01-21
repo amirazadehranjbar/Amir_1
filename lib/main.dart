ce601498fc03d96561a9d969db1a1522eaeec443
amirazadehranjbar <amirazadehranjbar@gmail.com>
2024-01-21 08:54:27 +0330

    Set Fonts and String To gen Plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:git_project/gen/assets.gen.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +16,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Scaffold(backgroundColor: Colors.white12),
+      theme: ThemeData(fontFamily: "Dubia",
+      textTheme: TextTheme(
+        bodySmall: TextStyle(color: Colors.white , fontFamily: "Dubia",fontSize: 12,fontWeight: FontWeight.w100),
+        bodyMedium: TextStyle(color: Colors.white , fontFamily: "Dubia",fontSize: 14,fontWeight: FontWeight.w200),
+        displaySmall: TextStyle(color: Colors.white , fontFamily: "Dubia",fontSize: 116,fontWeight: FontWeight.w300),
+        displayLarge: TextStyle(color: Colors.white , fontFamily: "Dubia",fontSize: 18,fontWeight: FontWeight.w400),
+      ),brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(backgroundColor: Colors.white12,
+      body: Center(
+        child: Image.asset(Assets.images.logo.path),
+      )),
     );
   }
 }
---
+++ lib/strings.dart
@@ -0,0 +1,7 @@
+class MyStrings {
+  static const String viewHotestBlogs = "View Hottest Blogs";
+  static const String viewHotestPad = "View Hottest Podcasts";
+  static const String relatedArticles = "View Related Articles";
+  static const String subArticle = "Submit Your Article";
+  static const String subPad = "submit your Podcast";
+}
